c725b207ee954bbba4ac35aadae78a244b386dd3
LoraZahorzhevska <Zagorzhevskaya@gmail.com>
2021-08-29 19:29:35 +0300

    добавляет тестовые файлы

---
+++ node-module/index.js
@@ -0,0 +1,5 @@
+const users = require("./users");
+
+console.log(users);
+
+// const { admins } = require("./users");
---
+++ node-module/users.js
@@ -0,0 +1,15 @@
+// console.log(new Date());
+
+const admins = ["Alex", "Andrey", "VAsiliy"];
+
+const clients = ["Anna", "ALina", "Tamara"];
+
+module.exports = {
+  admins,
+  clients,
+};
+
+// const users = {
+//   admins,
+//   clients,
+// };
